a71fd9c012596659a302ebeb1019b01b854271ef
Ivan Dejanovic <idejanovic@gmail.com>
2024-06-07 21:47:31 +0200

    Updated project setup. Added callback.

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,29 @@
-use yew::prelude::*;
+use yew::{function_component, html, Html, Properties, props, Callback, virtual_dom::AttrValue};
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    #[prop_or(AttrValue::from("Bob"))]
+    pub name: AttrValue,
+}
+
+#[function_component(HelloWorld)]
+fn hello_world(props: &Props) -> Html {
+    let onclick = Callback::from(move |_| {
+        let greeting = String::from("Hi there");
+        web_sys::console::log_1(&greeting.into()); // if uncommented will print
+    });
+
+    html! {
+        <div>
+          <p>{"Hello world "}{props.name.clone()}</p>
+          <button {onclick}>{ "Click" }</button>
+        </div>}
+}
+
 
 #[function_component(App)]
 pub fn app() -> Html {
-    html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
-    }
+    
+
+    html! {<HelloWorld />}
 }
