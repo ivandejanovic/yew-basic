7b72f1479421f810171532d1f79e14877fc86a7b
Ivan Dejanovic <idejanovic@gmail.com>
2024-06-17 14:18:35 +0200

    Added new greeting.

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use yew::{function_component, html, Html, Properties, props, Callback, virtual_dom::AttrValue};
+use yew::{function_component, html, use_state, virtual_dom::AttrValue, Callback, Html, Properties};
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
@@ -8,22 +8,27 @@ pub struct Props {
 
 #[function_component(HelloWorld)]
 fn hello_world(props: &Props) -> Html {
-    let onclick = Callback::from(move |_| {
-        let greeting = String::from("Hi there");
-        web_sys::console::log_1(&greeting.into()); // if uncommented will print
-    });
+    let message = use_state(|| AttrValue::from("Hello "));
+
+    let onclick = {
+        let message = message.clone();
+        Callback::from(move |_| {
+            let greeting = String::from("Hi there");
+            message.set(AttrValue::from("Ola "));
+            web_sys::console::log_1(&greeting.into());
+        })
+    };
 
     html! {
         <div>
-          <p>{"Hello world "}{props.name.clone()}</p>
+          <p>{(*message).clone()}{props.name.clone()}</p>
           <button {onclick}>{ "Click" }</button>
-        </div>}
+        </div>
+    }
 }
 
 
 #[function_component(App)]
 pub fn app() -> Html {
-    
-
-    html! {<HelloWorld />}
+    html! {<HelloWorld name={"Jake"}/>}
 }
